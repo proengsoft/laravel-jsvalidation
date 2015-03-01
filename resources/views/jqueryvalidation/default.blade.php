c1d7d3f7c64f9a34a848cc78dfa9e26670f8632e
Albert Moreno <a.moreno@proengsoft.com>
2015-03-02 00:52:21 +0100

    views

--- resources/views/jqueryvalidation/default.blade.php
+++ resources/views/jqueryvalidation/default.blade.php
@@ -2,15 +2,7 @@
 <script>
     jQuery(document).ready(function(){
 
-        $({{ $validator['selector'] }}).validate({
-            highlight: function (element) { // hightlight error inputs
-                $(element)
-                        .closest('.form-group').addClass('has-error'); // set error class to the control group
-            },
-            unhighlight: function (element) { // revert the change done by hightlight
-                $(element)
-                        .closest('.form-group').removeClass('has-error'); // set error class to the control group
-            },
+        $("{{ $validator['selector'] }}").validate({
             errorElement: 'span', //default input error message container
             errorClass: 'help-block help-block-error', // default input error message class
             focusInvalid: false, // do not focus the last invalid input
