67ddc3772d90f5d36b3ff5087d6169ba8a488516
Samuel Fernández <samuel84fa@gmail.com>
2015-07-05 12:44:46 +0200

    Added control to focus on components when the form has some errors

--- config/jsvalidation.php
+++ config/jsvalidation.php
@@ -13,4 +13,17 @@ return array(
      */
     'form_selector' => 'form',
 
+
+    /**
+     * If you change the focus on detect some error then active
+     * this parameter to move the focus to the first error found.
+     */
+    'focus_on_error' => false,
+
+    /**
+     * Duration time for the animation when We are moving the focus
+     * to the first error, http://api.jquery.com/animate/ for more information.
+     */
+    'duration_animate' => 1000,
+
 );
