1742ac8ba71836a9da0ec33dcb603f8c2a0dcad4
Sean Davidson <sean@thepottingsheddesign.com>
2015-07-28 11:20:10 +0100

    A simpler solution which looks for div.has-error and appends error

--- resources/views/bootstrap.php
+++ resources/views/bootstrap.php
@@ -28,23 +28,7 @@
             },
             <?php endif; ?>
             errorPlacement: function(error, element) {
-                if( element.is('select') && element.hasClass('select2-hidden-accessible')) {
-                    element.parent().append(error);
-                } else {
-                    if (element.attr("type") == "radio") {
-                        error.insertAfter(element.parents('div').find('.radio-list'));
-                    }
-                   else if (element.attr("type") == "checkbox") {
-                        error.insertAfter(element.parents('label'));
-                    }
-                    else {
-                        if(element.parent('.input-group').length) {
-                            error.insertAfter(element.parent());
-                        } else {
-                            error.insertAfter(element);
-                        }
-                    }
-                }
+               element.parents('div.has-error').append(error);
             },
             success: function (label) {
                 label
