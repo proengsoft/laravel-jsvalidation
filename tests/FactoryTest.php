6d3c3166b1c59373de9b45c91106d6aa1fea6a29
Albert Moreno <a.moreno@proengsoft.com>
2015-08-13 03:57:24 +0200

    Removing dependecies

--- tests/FactoryTest.php
+++ tests/FactoryTest.php
@@ -1,16 +1,30 @@
-<?php namespace Proengsoft\JsValidation\Test;
+<?php
+
+namespace  Illuminate\Foundation\Http;
+if (!class_exists('FormRequest')) {
+
+    class  FormRequest {
+        public function initialize(){}
+        public function setSession(){}
+        public function setUserResolver(){}
+        public function setRouteResolver() {}
+    }
+}
+
+
+namespace Proengsoft\JsValidation\Test;
 
 use Mockery as m;
 use Proengsoft\JsValidation\Exceptions\FormRequestArgumentException;
 use Proengsoft\JsValidation\Factory;
 
 
-class FakeFormRequest extends \Illuminate\Foundation\Http\FormRequest
-{
+class  FakeFormRequest extends \Illuminate\Foundation\Http\FormRequest {
     public function rules(){return ['name'=>'require'];}
     public function messages(){return [];}
-
+    public function attributes(){return [];}
 }
+
 
 
 class FactoryTest extends \PHPUnit_Framework_TestCase {
