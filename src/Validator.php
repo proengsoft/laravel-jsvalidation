cdac04729585dfb59dd51c54abd7ff156a7adc43
Cosmin Magureanu <gaffel2024@gmail.com>
2015-08-25 14:36:52 +0300

    Cleanup - fixes the validation messages as well

--- src/Validator.php
+++ src/Validator.php
@@ -51,18 +51,9 @@ class Validator extends BaseValidator
         // Check if JS Validation is disabled for this attribute
         $vAttributes = array_filter(array_keys($this->rules), [$this, 'jsValidationEnabled']);
         $vRules = array_intersect_key($this->rules, array_flip($vAttributes));
-        
-        // Support multidimensional array validation.
-        $attributeNames = array_map(function($v) {
-            $multiDimension = explode(".", $v);
-            if(count($multiDimension) > 1) {
-                return $multiDimension[0] . "[".implode("][", array_slice($multiDimension, 1)) . "]";
-            }
-            return $v;
-        }, array_keys($vRules));
 
         // Convert each rules and messages
-        $convertedRules = array_map([$this, 'jsConvertRules'], $attributeNames, $vRules);
+        $convertedRules = array_map([$this, 'jsConvertRules'], array_keys($vRules), $vRules);
 
         $convertedRules = array_filter($convertedRules, function ($value) {
             return !empty($value['rules']);
@@ -121,6 +112,7 @@ class Validator extends BaseValidator
     {
         $method = "jsRule{$rule}";
         $jsRule = false;
+        $attribute = $this->getAttributeName($attribute);
 
         if ($this->isRemoteRule($rule)) {
             list($attribute, $parameters) = $this->jsRemoteRule($attribute);
@@ -210,6 +202,22 @@ class Validator extends BaseValidator
             'rules' => $jsValidations,
             'messages' => $jsMessages,
         ];
+    }
+
+    /**
+     * Handles multidimensional attribute names
+     *
+     * @param $attribute
+     * @return string
+     */
+    private function getAttributeName($attribute)
+    {
+        $attributeArray = explode(".", $attribute);
+        if(count($attributeArray) > 1) {
+            return $attributeArray[0] . "[".implode("][", array_slice($attributeArray, 1)) . "]";
+        }
+
+        return $attribute;
     }
 
 
