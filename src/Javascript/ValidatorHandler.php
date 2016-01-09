b0075271eed0e5e5755b318b6c3a1c4357eb033c
albertmoreno <albert.moreno.t@gmail.com>
2016-01-08 20:46:39 -0500

    Applied fixes from StyleCI

--- src/Javascript/ValidatorHandler.php
+++ src/Javascript/ValidatorHandler.php
@@ -59,7 +59,7 @@ class ValidatorHandler
     /**
      *  Enable or disables remote validations.
      *
-     * @param boolean $enabled
+     * @param bool $enabled
      */
     public function setRemote($enabled)
     {
@@ -117,7 +117,7 @@ class ValidatorHandler
      * Check if rule should be validated with javascript.
      *
      * @param $jsRule
-     * @param boolean $includeRemote
+     * @param bool $includeRemote
      * @return bool
      */
     protected function isValidatable($jsRule, $includeRemote)
