2afecdd429d617e54c3e8f689b223e5330652e16
a-komarev <ell@cybercog.su>
2018-04-18 01:28:01 +0300

    Fix array rule validation

--- resources/assets/js/validations.js
+++ resources/assets/js/validations.js
@@ -339,6 +339,10 @@ $.extend(true, laravelValidation, {
          * Validate that an attribute is an array.
          */
         Array: function(value) {
+            if (element.name.indexOf('[') !== -1 && element.name.indexOf(']') !== -1) {
+                return true;
+            }
+
             return $.isArray(value);
         },
 
