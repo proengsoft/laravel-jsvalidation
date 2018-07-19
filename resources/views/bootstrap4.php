677e054910ffe661256b02ed0eb4fd0e94c42fa2
Leonardo Leal da Rosa <leonardodarosa23@gmail.com>
2018-07-19 09:24:25 -0300

    Multiple forms with same selector (#337)
    Fixes #336 Multiple forms with same selector

--- resources/views/bootstrap4.php
+++ resources/views/bootstrap4.php
@@ -1,54 +1,56 @@
 <script>
     jQuery(document).ready(function(){
 
-        $("<?= $validator['selector']; ?>").validate({
-            errorElement: 'span',
-            errorClass: 'invalid-feedback',
+        $("<?= $validator['selector']; ?>").each(function() {
+            $(this).validate({
+                errorElement: 'span',
+                errorClass: 'invalid-feedback',
 
-            errorPlacement: function(error, element) {
-                if (element.parent('.input-group').length ||
-                    element.prop('type') === 'checkbox' || element.prop('type') === 'radio') {
-                    error.insertAfter(element.parent());
-                    // else just place the validation message immediately after the input
-                } else {
-                    error.insertAfter(element);
-                }
-            },
-            highlight: function(element) {
-                $(element).closest('.form.control').removeClass('is-valid').addClass('is-invalid'); // add the Bootstrap error class to the control group
-            },
+                errorPlacement: function (error, element) {
+                    if (element.parent('.input-group').length ||
+                        element.prop('type') === 'checkbox' || element.prop('type') === 'radio') {
+                        error.insertAfter(element.parent());
+                        // else just place the validation message immediately after the input
+                    } else {
+                        error.insertAfter(element);
+                    }
+                },
+                highlight: function (element) {
+                    $(element).closest('.form.control').removeClass('is-valid').addClass('is-invalid'); // add the Bootstrap error class to the control group
+                },
 
-            <?php if (isset($validator['ignore']) && is_string($validator['ignore'])): ?>
+                <?php if (isset($validator['ignore']) && is_string($validator['ignore'])): ?>
 
-            ignore: "<?= $validator['ignore']; ?>",
-            <?php endif; ?>
+                ignore: "<?= $validator['ignore']; ?>",
+                <?php endif; ?>
 
-            /*
-             // Uncomment this to mark as validated non required fields
-             unhighlight: function(element) {
-             $(element).closest('.form.control').removeClass('is-invalid').addClass('is-valid');
-             },
-             */
-            success: function(element) {
-                $(element).closest('.form.control').removeClass('is-invalid').addClass('is-valid'); // remove the Boostrap error class from the control group
-            },
+                /*
+                 // Uncomment this to mark as validated non required fields
+                 unhighlight: function(element) {
+                 $(element).closest('.form.control').removeClass('is-invalid').addClass('is-valid');
+                 },
+                 */
+                success: function (element) {
+                    $(element).closest('.form.control').removeClass('is-invalid').addClass('is-valid'); // remove the Boostrap error class from the control group
+                },
 
-            focusInvalid: false, // do not focus the last invalid input
-            <?php if (Config::get('jsvalidation.focus_on_error')): ?>
-            invalidHandler: function(form, validator) {
+                focusInvalid: false, // do not focus the last invalid input
+                <?php if (Config::get('jsvalidation.focus_on_error')): ?>
+                invalidHandler: function (form, validator) {
 
-                if (!validator.numberOfInvalids())
-                    return;
+                    if (!validator.numberOfInvalids())
+                        return;
 
-                $('html, body').animate({
-                    scrollTop: $(validator.errorList[0].element).offset().top
-                }, <?= Config::get('jsvalidation.duration_animate') ?>);
-                $(validator.errorList[0].element).focus();
+                    $('html, body').animate({
+                        scrollTop: $(validator.errorList[0].element).offset().top
+                    }, <?= Config::get('jsvalidation.duration_animate') ?>);
+                    $(validator.errorList[0].element).focus();
 
-            },
-            <?php endif; ?>
+                },
+                <?php endif; ?>
 
-            rules: <?= json_encode($validator['rules']); ?>
-        })
-    })
+                rules: <?= json_encode($validator['rules']); ?>
+            });
+        });
+    });
 </script>
