a8db82bb36ac40eaa2b2c722e2d9b258b2fe734c
FoxDev <xuandung38@gmail.com>
2019-09-08 22:08:54 +0700

    add support for uikit

--- config/jsvalidation.php
+++ config/jsvalidation.php
@@ -5,7 +5,7 @@ return [
     /*
      * Default view used to render Javascript validation code
      *
-     * Supported: 'jsvalidation::bootstrap', 'jsvalidation::bootstrap4'
+     * Supported: 'jsvalidation::bootstrap', 'jsvalidation::bootstrap4', 'jsvalidation::uikit'
      */
     'view' => 'jsvalidation::bootstrap',
 
