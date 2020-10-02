2a13aa4fdeeb93aa848783db055bc7bcedf0d4d0
Kieran Brahney <kieran@supportpal.com>
2020-10-02 17:26:01 +0100

    fix tests

--- tests/stubs/JsValidatorFactoryTest.php
+++ tests/stubs/JsValidatorFactoryTest.php
@@ -1,51 +1,15 @@
 <?php
 
-namespace  Illuminate\Foundation\Http {
-    if (!class_exists('FormRequest')) {
-        class  FormRequest {
-            public function initialize(){}
-            public function setLaravelSession(){}
-            public function setUserResolver(){}
-            public function setRouteResolver() {}
-            public function setContainer() {}
-        }
-    }
-}
-
 namespace Proengsoft\JsValidation\Tests {
-    use Mockery as m;
-
     class  StubFormRequest extends \Illuminate\Foundation\Http\FormRequest {
         public function rules(){return ['name'=>'require'];}
         public function messages(){return [];}
         public function attributes(){return [];}
-
-        public static function createFromBase() {
-            //$sessionMock =  m::mock('Symfony\Component\HttpFoundation\Session\SessionInterface',[]);
-            $mockedRequest = m::mock(\Symfony\Component\HttpFoundation\Request::class);
-            $mockedRequest->shouldReceive('setSession')
-                ->shouldReceive('setUserResolver')
-                ->shouldReceive('setRouteResolver')
-                ->shouldReceive('messages')->andReturn([])
-                ->shouldReceive('attributes')->andReturn([]);
-            return $mockedRequest;
-        }
     }
 
     class StubFormRequest2 extends \Proengsoft\JsValidation\Remote\FormRequest {
         public function rules(){return ['name'=>'require'];}
         public function messages(){return [];}
         public function attributes(){return [];}
-
-        public static function createFromBase() {
-            //$sessionMock =  m::mock('Symfony\Component\HttpFoundation\Session\SessionInterface',[]);
-            $mockedRequest = m::mock(\Symfony\Component\HttpFoundation\Request::class);
-            $mockedRequest->shouldReceive('setSession')
-                ->shouldReceive('setUserResolver')
-                ->shouldReceive('setRouteResolver')
-                ->shouldReceive('messages')->andReturn([])
-                ->shouldReceive('attributes')->andReturn([]);
-            return $mockedRequest;
-        }
     }
 }
