549139ae6a8285c8aaa1a86e59b07ed42c780b6a
mankowitz <scott@mankowitz.org>
2021-09-16 13:59:49 -0400

    fix: after rule when date is supplied (#737)
    * short circuit parseTime for unix values
    
    * typo
    
    * Update resources/assets/js/helpers.js
    
    Co-authored-by: Kieran <kieran.brahney@gmail.com>
    Co-authored-by: Kieran <kieran@supportpal.com>

--- resources/assets/js/helpers.js
+++ resources/assets/js/helpers.js
@@ -183,6 +183,10 @@ $.extend(true, laravelValidation, {
             var timeValue = false;
             var fmt = new DateFormatter();
 
+            if (typeof value === 'number' && typeof format === 'undefined') {
+                return value;
+            }
+
             if (typeof format === 'object') {
                 var dateRule = this.getLaravelValidation('DateFormat', format);
                 if (dateRule !== undefined) {
